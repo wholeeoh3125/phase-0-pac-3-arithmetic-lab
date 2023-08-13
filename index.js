2e9296c6c287b79a385e82b37911e504ba6e2bc0
Julio <julioantjimenez@gmail.com>
2023-08-13 15:08:53 -0400

    arithmetics lab assignment

---
+++ index.js
@@ -0,0 +1,37 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function increment(n) {
+    return (n + 1)
+}
+function decrement(n) {
+    return (n - 1)
+}
+
+function makeInt(n) {
+    return n;
+}
+parseInt(n);
+
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+parseInt(n);
+
+function preserveDecimal(n) {
+    return parseFloat(n, 10);
+}
+parseFloat(n);
